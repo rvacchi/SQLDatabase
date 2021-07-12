4da29d67ee6bc5a001f520f93f784cf777981698
Rodolfo Soares <rsvacchi@gmail.com>
2021-07-11 23:28:46 -0300

    comit
    agora vai

---
+++ SqlServerDatabase/Product.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Product]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Name] VARCHAR(50) NULL, 
+    [Cost] INT NULL, 
+    [Color] NVARCHAR(50) NULL, 
+    [Rodolfo] NCHAR(10) NULL, 
+    [maisuma] NCHAR(10) NULL, 
+    [asdasda] NCHAR(10) NULL, 
+    [sdsdf] NCHAR(10) NULL, 
+    [ffff] NCHAR(10) NULL 
+)
