fd8689bcf053c48fb7b202835db29799ff1df4e4
Rodolfo Soares <rsvacchi@gmail.com>
2021-07-11 23:30:41 -0300

    limpei geral

--- SqlServerDatabase/Product.sql
+++ SqlServerDatabase/Product.sql
@@ -4,9 +4,5 @@
     [Name] VARCHAR(50) NULL, 
     [Cost] INT NULL, 
     [Color] NVARCHAR(50) NULL, 
-    [Rodolfo] NCHAR(10) NULL, 
-    [maisuma] NCHAR(10) NULL, 
-    [asdasda] NCHAR(10) NULL, 
-    [sdsdf] NCHAR(10) NULL, 
-    [ffff] NCHAR(10) NULL 
+    [Rodolfo] NCHAR(10) NULL 
 )
